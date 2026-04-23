219186439cb5713795f0fa8420ba80ebaf4edef9
zelda <shwarzbergzelda@gmail.com>
2026-04-23 12:47:36 -0400

    hide password

--- server/queries.js
+++ server/queries.js
@@ -1,13 +1,15 @@
 // Connect to Postgres using the node-postgres package
 
-const POOL = require('pg').Pool
+require('dotenv').config()
 
-const pool = new POOL({
-    user: 'me',
-    host: 'localhost',
-    database: 'favlinks',
-    password: 'password',
-    port: 5432
+const { Pool } = require('pg')
+
+const pool = new Pool({
+    user: process.env.PGUSER,
+    host: process.env.PGHOST,
+    database: process.env.PGDATABASE,
+    password: process.env.PGPASSWORD,
+    port: Number(process.env.PGPORT),  
 })
 
 // Create all the functions that will be our request handlers in our Express server
